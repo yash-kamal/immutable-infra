278940f183c03a254d867b1c3b26827f29ff1c9e
yash-kamal <kamal2002yash@gmail.com>
2025-11-27 00:04:57 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,5 @@
-variable "ami_id" {
-  type = string
-}
+variable "aws_region" { default = "us-east-1" }
+variable "vpc_id" { }
+variable "public_subnets" { type = list(string) }
+variable "ami_id" { description = "AMI/image id built by packer" }
+variable "instance_type" { default = "t3.micro" }
