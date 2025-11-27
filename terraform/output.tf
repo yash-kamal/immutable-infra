3e6716bc533d3f7ba6e7e4e3f04ccacb2b895a58
yash-kamal <kamal2002yash@gmail.com>
2025-11-27 11:45:50 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,13 @@
+output "alb_listener_arn" {
+  value       = aws_lb_listener.app_listener.arn
+  description = "ALB listener ARN for switching traffic."
+}
+
+output "primary_target_group_arn" {
+  value       = aws_lb_target_group.primary.arn
+  description = "Primary (blue) target group ARN."
+}
+
 output "public_ip" {
   value = aws_instance.app.public_ip
 }
