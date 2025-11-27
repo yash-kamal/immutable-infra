e98914496b61759f4bdf335c8bbc6d5b89a94356
yash-kamal <kamal2002yash@gmail.com>
2025-11-27 11:44:49 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,20 @@
+variable "deployment_strategy" {
+  description = "Deployment strategy: blue-green | canary | rolling"
+  type        = string
+  default     = "blue-green"
+}
+
+variable "canary_percent" {
+  description = "Percentage of traffic to route to canary in canary deployments"
+  type        = number
+  default     = 10
+}
+
+variable "rolling_batch_size" {
+  description = "MinHealthyPercentage or batch size for instance refresh"
+  type        = number
+  default     = 90
+}
 variable "aws_region" { default = "us-east-1" }
 variable "vpc_id" { }
 variable "public_subnets" { type = list(string) }
